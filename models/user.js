5fc8cfc6c72651d2428a01cb75c09e0782d5cf0b
woyzeck <bastip92@gmail.com>
2018-11-23 14:32:37 +0000

    add user profile feature

--- models/user.js
+++ models/user.js
@@ -5,6 +5,10 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: {type: String, default: "https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg"},
+    firstName: String,
+    lastName: String,
+    email: String,
     isAdmin: {type: Boolean, default: false}
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express  = require("express");
 var router   = express.Router();
 var passport = require("passport");
 var User     = require("../models/user");
+var Campground = require("../models/campground");
 
 //root route
 router.get("/", function(req, res){
@@ -14,7 +15,14 @@ router.get("/register", function(req, res){
 });
 //handle signup logic
 router.post("/register", function(req, res){
-   var newUser = new User({username: req.body.username});
+   var newUser = new User(
+      {
+         username: req.body.username, 
+         firstName: req.body.firstName,
+         lastName: req.body.lastName,
+         email: req.body.email,
+         avatar: req.body.avatar
+      });
    if(req.body.adminCode === 'secretcode123') {
       newUser.isAdmin = true;
    }
@@ -42,11 +50,29 @@ router.post("/login", passport.authenticate("local",
         failureRedirect: "/login"
     }), function(req, res){
 });
+
 //logout route
 router.get("/logout", function(req, res){
    req.logout();
    req.flash("success", "Logged you out!");
    res.redirect("/campgrounds");
+});
+
+// user profile
+router.get("/users/:id", function(req, res){
+   User.findById(req.params.id, function(err, foundUser){
+      if(err){
+         req.flash("error", "user not found");
+         return res.redirect("/");
+      }
+      Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+         if(err){
+            req.flash("error", "something went wrong");
+            return res.redirect("/");
+         }
+         res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+      });
+   });
 });
 
 module.exports = router;
