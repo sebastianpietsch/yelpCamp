3e94bb8e6ce58737a641d6aaed7733d639123c95
woyzeck <bastip92@gmail.com>
2018-11-27 09:50:38 +0000

    add admincode to ENV

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-var express  = require("express");
-var router   = express.Router();
-var passport = require("passport");
-var User     = require("../models/user");
+var express    = require("express");
+var router     = express.Router();
+var passport   = require("passport");
+var User       = require("../models/user");
 var Campground = require("../models/campground");
 var async      = require("async");
 var nodemailer = require("nodemailer");
@@ -26,7 +26,7 @@ router.post("/register", function(req, res){
          email: req.body.email,
          avatar: req.body.avatar
       });
-   if(req.body.adminCode === 'secretcode123') {
+   if(req.body.adminCode === process.env.ADMINCODE) {
       newUser.isAdmin = true;
    }
    User.register(newUser, req.body.password, function(err, user){
@@ -62,8 +62,56 @@ router.get("/forgot", function(req, res){
    res.render("forgot");
 });
 
-router.post("/forgot", function(req, res){
-   
+router.post("/forgot", function(req, res, next){
+   async.waterfall([
+      function(done) {
+         crypto.randomBytes(20, function(err, buf){
+            var token = buf.toString('hex');
+            done(err, token);
+         });
+      },
+      function(token, done){
+         User.findOne({email: req.body.email}, function(err, user){
+            if(!user){
+               req.flash("error", "No account with that email address exists.");
+               return res.redirect("/forgot");
+            }
+            
+            user.resetPasswordToken = token;
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+            
+            user.save(function(err){
+               done(err, token, user);
+            });
+         });
+      },
+      function(token, user, done){
+         var smtpTransport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+               user: "bastip92@gmail.com",
+               pass: process.env.GMAILPW
+            }
+         });
+         var mailOptions = {
+            to: user.email,
+            from:    'bastip92@gmail.com',
+            subject: 'YelpCamp Password Reset',
+            text:    'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                     'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+         };
+         smtpTransport.sendMail(mailOptions, function(err){
+            console.log('mail sent');
+            req.flash("success", "An e-mail has ben sent to " + user.email + " with further instructions.");
+            done(err, "done")
+         });
+      }
+   ], function(err){
+      if(err) return next(err);
+      res.redirect("/forgot");
+   });
 });
 
 module.exports = router;
